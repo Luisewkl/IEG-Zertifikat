413ebb5faa977ecc78a7ccf6cd792d680f683082
Luisewkl <luise.winkeler@icloud.com>
2026-06-15 17:33:10 +0200

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,7 +0,0 @@
-/* IEG Claude Academy — Supabase Config */
-
-const SUPABASE_URL = 'https://hojkbskyhwocsknucvos.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhvamtic2t5aHdvY3NrbnVjdm9zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE1MzI4NzYsImV4cCI6MjA5NzEwODg3Nn0.A2jd2EPn9bSBHiUh-CQDbx-zUnGart4iU688gXypT3c';
-
-window._supabaseURL = SUPABASE_URL;
-window._supabaseKEY = SUPABASE_ANON_KEY;
